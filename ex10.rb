1ad1d6e74d921c9d8335e3324194e9442606ac4b
Nayed Saïd Ali <nayed.saidali@gmail.com>
2016-10-28 20:38:58 +0200

    ex10

---
+++ ex10.rb
@@ -0,0 +1,23 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishied
+\t* Catnip\n\t* Grass
+"""
+
+fat_cat2 = '''
+I\'ll do another list:
+\t* Cat ball
+\t* Cat meow
+\t* Cat\n\t* Grass
+'''
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
+puts fat_cat2
