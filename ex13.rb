0854455db3c1e534d006c060ecd76d706e29587e
Nayed Saïd Ali <nayed.saidali@gmail.com>
2016-10-29 21:52:26 +0200

    ex13

---
+++ ex13.rb
@@ -0,0 +1,5 @@
+first, second, third = ARGV
+
+puts "Your first variable is: #{first}" unless first.nil?
+puts "Your second variable is: #{second}" unless second.nil?
+puts "Your third variable is: #{third}" unless third.nil?
